afefcf0c7e9e5e2635fc48fd31087975acb4e5f6
Aaron I Trierweiler <ait530@users.noreply.github.com>
2017-06-23 12:25:00 -0500

    adding dependencies and made nytreact mongo database

---
+++ server.js
@@ -0,0 +1,5 @@
+// Dependencies
+var express = require("express");
+var mongoose = require("mongoose");
+// var expressHandlebars = require("express-handlebars");
+var bodyParser = require("body-parser");
